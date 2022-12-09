4ad308c648b1a9b8a24c4f8694cb818b5af80209
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-12-09 22:16:36 +0530

    Added:Logic for display the welcome meassge of hashtable program

--- src/com/java/hashtable/HashTable.java
+++ src/com/java/hashtable/HashTable.java
@@ -11,34 +11,9 @@ import java.util.Set;
  */
 public class HashTable {
 	/*
-	 * Method Name :userInputSentence
-	 * 
-	 */
-	static void userInputSentence(String userString) {
-		HashMap<String, Integer> frequency = new HashMap<String, Integer>();
-		String[] words = userString.split(" ");
-		/*
-		 * using forEach loop for iterating the given words
-		 */
-		for (String word : words) {
-			if (frequency.containsKey(word)) {
-				frequency.put(word, frequency.get(word) + 1);
-			} else {
-				frequency.put(word, 1);
-			}
-		}
-		Set<String> stringFrequency = frequency.keySet();
-		System.out.println("Frequency Of Each word in Sentence: \n 'To Be Or Not To Be'");
-		for (String word : stringFrequency) {
-			System.out.println(word + " = " + frequency.get(word) + " times.");
-
-		}
-	}
-
-	/*
 	 * Program execution starts from main method
 	 */
 	public static void main(String[] args) {
-		userInputSentence("To Be Or Not To Be");
+		System.out.println("Welcome to the HashTable program");
 	}
 }
